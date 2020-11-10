6daf95d2a1bbd3bd8c7fc1a05e47ed6f76922ca5
jedy <cocota93@gmail.com>
2020-11-10 12:38:32 +0900

    BaseTimeEntity제대로 동작 안하는현상 수정

--- blahblah-app-admin/src/main/java/org/jedy/BlahblahAppAdminApplication.java
+++ blahblah-app-admin/src/main/java/org/jedy/BlahblahAppAdminApplication.java
@@ -2,8 +2,10 @@ package org.jedy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BlahblahAppAdminApplication {
 
   public static void main(String[] args) {
--- blahblah-app-api/src/main/java/org/jedy/BlahblahAppApiApplication.java
+++ blahblah-app-api/src/main/java/org/jedy/BlahblahAppApiApplication.java
@@ -2,8 +2,10 @@ package org.jedy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BlahblahAppApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlahblahAppApiApplication.class, args);
