3ddd133ae11c0c7a642a7b02dcebdce36e7f0a34
jedy <cocota93@gmail.com>
2020-11-10 20:41:42 +0900

    기본 페이징 예제

--- blahblah-app-admin/src/main/java/org/jedy/member/controller/MemberController.java
+++ blahblah-app-admin/src/main/java/org/jedy/member/controller/MemberController.java
@@ -2,15 +2,20 @@ package org.jedy.member.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.jedy.member.dto.MemberResponse;
+import org.jedy.member.dto.MemberSearchCondition;
 import org.jedy.member.exception.MemberNotFoundException;
 import org.jedy.member.exception.MemberSomethingException;
+import org.jedy.member.repository.query.MemberQueryRepository;
 import org.jedy.member_core.domain.Member;
 import org.jedy.member_core.repository.MemberRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -19,12 +24,22 @@ import java.util.Optional;
 public class MemberController {
 
   private final MemberRepository memberRepository;
+  private final MemberQueryRepository memberQueryRepository;
 
 
   @GetMapping
   public String getPage(Model model) {
     model.addAttribute("name", "jedy");
     return "hihi";
+  }
+
+
+  @GetMapping("/list")
+  public String getList(Model model, MemberSearchCondition condition, Pageable pageable) {
+    Page<MemberResponse> pageList = memberQueryRepository.searchPageComplex(condition, pageable);
+    model.addAttribute("pageList", pageList);
+
+    return "members/list";
   }
 
 
