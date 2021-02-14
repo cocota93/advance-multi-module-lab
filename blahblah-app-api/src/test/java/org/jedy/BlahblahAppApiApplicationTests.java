d067ede9d58b32b820a402c1f08708cb4b37e854
jedy <cocota93@gmail.com>
2021-02-14 21:09:44 +0900

    rds연동중

--- blahblah-app-api/src/test/java/org/jedy/BlahblahAppApiApplicationTests.java
+++ blahblah-app-api/src/test/java/org/jedy/BlahblahAppApiApplicationTests.java
@@ -1,11 +1,24 @@
 package org.jedy;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BlahblahAppApiApplicationTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
     void contextLoads() {
+//        when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("helo test~");
     }
 }
