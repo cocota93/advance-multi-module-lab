17204a3bc6360e9516129425b62ea37b70f6f236
jedy <cocota93@gmail.com>
2021-06-11 20:48:00 +0900

    q파일 제대로 생성 안되는 문제 수정 참고 : https://frogand.tistory.com/m/41?category=850904

--- blahblah-app-admin/src/main/java/org/jedy/notice/repository/query/NoticeQueryRepositoryImpl.java
+++ blahblah-app-admin/src/main/java/org/jedy/notice/repository/query/NoticeQueryRepositoryImpl.java
@@ -2,29 +2,15 @@ package org.jedy.notice.repository.query;
 
 
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQuery;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.jedy.member.dto.MemberResponse;
-import org.jedy.member.dto.MemberSearchCondition;
-import org.jedy.member.dto.QMemberResponse;
-import org.jedy.member.repository.query.MemberQueryRepositoryCustom;
-import org.jedy.member_core.domain.Member;
 import org.jedy.notice.dto.NoticeSearchCondition;
 import org.jedy.notice_core.domain.Notice;
-import org.jedy.notice_core.domain.QNotice;
 import org.jedy.system_core.support.Querydsl4RepositorySupport;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.support.PageableExecutionUtils;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
-import static org.jedy.member_core.domain.QMember.member;
 import static org.jedy.notice_core.domain.QNotice.notice;
-import static org.springframework.util.StringUtils.hasText;
 
 public class NoticeQueryRepositoryImpl extends Querydsl4RepositorySupport implements NoticeQueryRepositoryCustom {
 
