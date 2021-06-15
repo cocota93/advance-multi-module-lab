9da17bc2e72b081368fad6e48802d69ae25abfa8
jedy <cocota93@gmail.com>
2021-06-15 23:00:55 +0900

    서버 올라갈때 테스트를 위한 기본데이터 추가(aws에서 하나의 인스턴스만 기본데이터 생성할수 있도록 initdb라는 프로필 가졌을때만 생성하도록)

--- boot-shop-app-api/src/main/java/org/jedy/initDb.java
+++ boot-shop-app-api/src/main/java/org/jedy/initDb.java
@@ -1,23 +1,31 @@
 package org.jedy;
 
 import lombok.RequiredArgsConstructor;
+import org.jedy.category.dto.request.CategoryRequestDto;
+import org.jedy.category.repository.CategoryRepository;
+import org.jedy.category.service.CategoryService;
 import org.jedy.member.domain.Member;
 import org.jedy.member.repository.MemberRepository;
+import org.jedy.product.dto.request.ProductRequestDto;
+import org.jedy.product.service.ProductService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
-import javax.persistence.EntityManager;
 
 @Component
 @RequiredArgsConstructor
+@Profile("initdb")
 public class initDb {
 
     private final InitService initService;
 
     @PostConstruct
     public void init(){
+        initService.categoryInit();
+        initService.productListInit();
 //        initService.memberInit();
     }
 
@@ -26,23 +34,57 @@ public class initDb {
     @Transactional
     @RequiredArgsConstructor
     static class InitService{
-        private final EntityManager em;
-
         private final PasswordEncoder passwordEncoder;
-
         private final MemberRepository memberRepository;
+        private final CategoryRepository categoryRepository;
 
-        public void memberInit(){
+        private final ProductService productService;
+        private final CategoryService categoryService;
+
+        public void categoryInit(){
+            {
+                CategoryRequestDto categoryRequestDto = new CategoryRequestDto();
+                categoryRequestDto.setCatName("전자제품");
+                categoryRequestDto.setCatCd("cd000001");
+                categoryService.addCategory(categoryRequestDto);
+            }
+            {
+                CategoryRequestDto categoryRequestDto = new CategoryRequestDto();
+                categoryRequestDto.setCatName("휴대폰");
+                categoryRequestDto.setCatCd("cd000002");
+                categoryRequestDto.setUpperCatCd("cd000001");
+                categoryService.addCategory(categoryRequestDto);
+            }
+
+            {
+                CategoryRequestDto categoryRequestDto = new CategoryRequestDto();
+                categoryRequestDto.setCatName("의류");
+                categoryRequestDto.setCatCd("cd000003");
+                categoryService.addCategory(categoryRequestDto);
+            }
+        }
+        public void productListInit() {
+            createProduct("갤럭시s7", 10000L, "cd000002");
+            createProduct("세탁기", 3000L, "cd000001");
+            createProduct("G-PRO2", 50000L, "cd000002");
+            createProduct("물빠진 청바지", 90000L, "cd000003");
+            createProduct("찢어진 청바지", 10000L, "cd000003");
+            createProduct("껌정 맨투맨", 30000L, "cd000003");
+        }
+        public void memberInit() {
             for(int i = 1; i < 3; ++i){
                 Member member = createMember("member", i);
-                em.persist(member);
+                memberRepository.save(member);
             }
         }
 
-        public void noticeInit(){
-//            Notice notice =
+        private void createProduct(String name, long price, String upperCatCd) {
+            ProductRequestDto productRequestDto = new ProductRequestDto();
+            productRequestDto.setName(name);
+            productRequestDto.setPrice(price);
+            productRequestDto.setUpperCatCd(upperCatCd);
+            productService.addProduct(productRequestDto);
         }
-
         private Member createMember(String aliasName, Integer sequence) {
             Member member = Member.BySignup()
                     .loginId(aliasName + sequence)
@@ -53,5 +95,9 @@ public class initDb {
                     .build();
             return member;
         }
+//        private Product createProduct(){
+////            productService.addProduct()
+//        }
+
     }
 }
